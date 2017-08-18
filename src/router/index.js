93f795113defd6ff57e20ecfc639ae74192ad40f
Gernot <Gernot@Gernots-MacBook-Pro.local>
2017-08-19 00:53:28 +0200

    production build

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Resource from 'vue-resource'
 import Hello from '@/components/Hello'
 
 Vue.use(Router)
+Vue.use(Resource)
 
 export default new Router({
   routes: [
